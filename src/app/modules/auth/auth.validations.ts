b6f23ef3a4d3f61590d5451b7a51b9da0b349dda
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-10-02 19:34:02 +0600

    Get All Users Only Allowed For Admin

---
+++ src/app/modules/auth/auth.validations.ts
@@ -0,0 +1,60 @@
+import { z } from 'zod';
+import { role } from './auth.constants';
+
+const create = z.object({
+  body: z.object({
+    name: z
+      .string({
+        required_error: 'Name is required',
+      })
+      .min(2)
+      .max(100),
+    email: z
+      .string({
+        required_error: 'Email is required',
+      })
+      .email(),
+    password: z.string().min(6).max(100),
+    role: z.enum([...role] as [string, ...string[]], {
+      required_error: 'Role is required!',
+    }),
+    contactNo: z
+      .string({
+        required_error: 'Contact number is required',
+      })
+      .min(3)
+      .max(11),
+    address: z.string().min(2).max(100),
+    profileImg: z.string().optional(),
+  }),
+});
+
+const login = z.object({
+  body: z.object({
+    email: z
+      .string({
+        required_error: 'Email is required',
+      })
+      .email(),
+    password: z
+      .string({
+        required_error: 'Password is required',
+      })
+      .min(6)
+      .max(100),
+  }),
+});
+
+const refreshToken = z.object({
+  cookies: z.object({
+    refreshToken: z.string({
+      required_error: 'Refresh Token is required',
+    }),
+  }),
+});
+
+export const AuthValidations = {
+  create,
+  login,
+  refreshToken,
+};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,14 +1,20 @@
 import express from 'express';
+import { AuthRoutes } from '../modules/auth/auth.routes';
+import { UserRoutes } from '../modules/users/users.routes';
 
 const router = express.Router();
 
-// const moduleRoutes = [
-//   // ... routes
-//   {
-//     path: '',
-//     routes: '',
-//   },
-// ];
+const moduleRoutes = [
+  // ... routes
+  {
+    path: '/auth',
+    routes: AuthRoutes,
+  },
+  {
+    path: '/users',
+    routes: UserRoutes,
+  },
+];
 
-// moduleRoutes.forEach(route => router.use(route.path, route.route));
+moduleRoutes.forEach(route => router.use(route.path, route.routes));
 export default router;
